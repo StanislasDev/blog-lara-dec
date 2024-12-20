9930bffbbc4b098649f92d1949eeb4dfa9154b33
Stanislas <simostanley91@gmail.com>
2024-12-20 06:08:01 +0100

    List des posts et boutton de création d'un post

--- app/Livewire/Posts.php
+++ app/Livewire/Posts.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class Posts extends Component
 {
+    public $showModalForm = false;
+
+    public function showCreatePostModal()
+    {
+        $this->showModalForm = true;
+    }
     public function render()
     {
         return view('livewire.posts');
